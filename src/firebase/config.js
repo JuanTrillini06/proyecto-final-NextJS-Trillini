66b88f3de9d05b1f1aad316aec9e81d4e6f5c9fc
JuanTrillini06 <juanjose_207@hotmail.com>
2025-06-24 21:40:10 -0300

    añadido envlocal

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDN7oXc8OBKqu7G_JIXQgTsMFViVtvNlxs",
-  authDomain: "juandrobenext.firebaseapp.com",
-  projectId: "juandrobenext",
-  storageBucket: "juandrobenext.firebasestorage.app",
-  messagingSenderId: "971456713185",
-  appId: "1:971456713185:web:45c66529ad7b07a42202a0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
